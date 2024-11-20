e5c742624e85e1bfd3117be8d689d3293a045443
xi6th <ejise45@gmail.com>
2024-11-20 22:46:05 +0100

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Custom Ncs
+
+A custom application for ncs
+
+#### License
+
+MIT
---
+++ custom_ncs/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Custom Ncs",
+			"type": "module",
+			"label": _("Custom Ncs")
+		}
+	]
